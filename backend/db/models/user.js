efcfa05323e40925225682e1c4708862ee32f350
AhmadMraish <ahmadmraish59@gmail.com>
2021-09-08 22:33:54 +0300

    CreateUser Schema Ticket, added the CreateUser Schema

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -3,10 +3,28 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
+const userSchema= mongoose.Schema({
+
+    firstName:{type:String,required:true} ,
+    lastName: {type:String}  ,
+    joinDate: {type:Date ,default:Date.now()} ,
+    age: {type:Number},
+    email: {type:String,required:true} ,
+    password: {type:String,required:true},
+    avatar:{type:String} ,
+    cover:  {type:String},
+    gender: {type:String, required:true},
+    album: [{type:String}],
+    followers: [{type:mongoose.Schema.Types.ObjectId,ref:"User"}],
+    followersCount:{type:Number},
+    folowees: [{type:mongoose.Schema.Types.ObjectId,ref:"User"}],
+    foloweesCount: {type:Number}
+
+});
+module.exports=mongoose.model("User",userSchema);
 
 
-
-
-
-
+// we need function pre save
+// avatar : if no avatar set default avatar and cover based on gender
+// maybe need function to update followers/folowees counters
 
