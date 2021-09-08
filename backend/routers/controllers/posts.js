22ab35527c8e1476de6f6725c0f66d7e9fa8eaf3
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-09-08 10:38:36 +0300

    create new post controller

--- backend/routers/controllers/posts.js
+++ backend/routers/controllers/posts.js
@@ -1,2 +1,18 @@
 const Post=require("../../db/models/posts");
 
+const createNewPost=(req,res)=>{
+    const {body,img,user}=req.body;
+    const newPost=new Post({
+        body,
+        img,
+        user
+    });
+
+    newPost
+    .save()
+    .then(result=>res.status(201).json({success:true,message:result}))
+    .catch((err)=>res.status(500).json({success:false,message:"Server Error"}));
+}
+
+
+module.exports=createNewPost;
--- backend/routers/routes/posts.js
+++ backend/routers/routes/posts.js
@@ -1,5 +1,4 @@
 const express=require("express");
-
 const postsRouter=express.Router();
 
 
