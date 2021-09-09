fec5349f5c06f72b9091d31b1c7921a9e68438b4
AhmadMraish <ahmadmraish59@gmail.com>
2021-09-09 04:48:34 +0300

    updatePostById route done and imported its function

--- backend/routers/routes/posts.js
+++ backend/routers/routes/posts.js
@@ -1,10 +1,11 @@
 const express=require("express");
 
-const {createNewPost,getAllPosts}=require("../controllers/posts");
+const {createNewPost,getAllPosts,updatePostById}=require("../controllers/posts");
+
 const postsRouter=express.Router();
 
 postsRouter.get('/',getAllPosts)
 postsRouter.post("/",createNewPost);
-
+postsRouter.put("/posts/:id",updatePostById)
 
 module.exports=postsRouter;
