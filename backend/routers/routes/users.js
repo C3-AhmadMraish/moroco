b01530621dd0f06bacb8f35ba31e37a8a0d5864e
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-09-09 18:00:04 +0300

    add follwoUnfollwo route in users.js routes

--- backend/routers/routes/users.js
+++ backend/routers/routes/users.js
@@ -1,8 +1,9 @@
 const express=require("express");
+const {getUserById,register,follwoUnfollwo} = require("../controllers/users");
 const usersRouter=express.Router();
 
 usersRouter.post("/",register);
 usersRouter.get("/:id",getUserById);
-
+usersRouter.put("/:id/follow",follwoUnfollwo);
 
 module.exports=usersRouter;
