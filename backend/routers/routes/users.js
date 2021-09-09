a37b73287e8522354d384d1c052b36cb8e7f4ae1
AhmadMraish <ahmadmraish59@gmail.com>
2021-09-09 05:40:22 +0300

    register route + imported its function

--- backend/routers/routes/users.js
+++ backend/routers/routes/users.js
@@ -1,7 +1,8 @@
 const express=require("express");
-const {getUserById} = require("../controllers/users")
+const {getUserById,register} = require("../controllers/users")
 const usersRouter=express.Router();
 
+usersRouter.post("/",register);
 usersRouter.get("/:id",getUserById);
 
 module.exports=usersRouter;
