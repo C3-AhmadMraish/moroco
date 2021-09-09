a74a4646d7dcbe78066d7130499e2a0c127055ec
AbdullahNaser94 <abdulahnaser94@gmail.com>
2021-09-09 18:42:37 +0300

    get all comment tiket done

--- backend/routers/controllers/comments.js
+++ backend/routers/controllers/comments.js
@@ -3,6 +3,28 @@ const Comment =require("../../db/models/comments");
 
 const getAllComments = (req, res) => {
 
+  const _id = req.params.id;
+  Comment.findById(_id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: `No comment Found with this ${_id}`,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: `The comment with ${_id}`,
+        post: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "server error",
+        err: err,
+      });
+    });
 
 }
 
