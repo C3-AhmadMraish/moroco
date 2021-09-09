3d511dd5f8c9d6d44ed711a4600f49acfd17c852
BasharAlZawahreh <86913003+BasharAlZawahreh@users.noreply.github.com>
2021-09-09 06:48:13 +0300

    Update posts.js

--- backend/routers/routes/posts.js
+++ backend/routers/routes/posts.js
@@ -1,11 +1,11 @@
 const express=require("express");
 
-const {createNewPost,getAllPosts,deletePostById}=require("../controllers/posts");
+const {createNewPost,getAllPosts}=require("../controllers/posts");
 const postsRouter=express.Router();
 
 postsRouter.get('/',getAllPosts)
 postsRouter.post("/",createNewPost);
-postsRouter.delete("/deletepost/:id",deletePostById);
+//postsRouter.delete("/deletepost/:id",deletePostById);   //deletePostById
 
 
 module.exports=postsRouter;
