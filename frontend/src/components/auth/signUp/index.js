0604395054d3d5ec3d2f7fe7d1824864ceb5e48c
AhmadMraish <ahmadmraish59@gmail.com>
2021-09-10 16:25:51 +0300

    Prepare register Component

---
+++ frontend/src/components/auth/signUp/index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import React, { useState} from "react";
+import {useHistory } from "react-router-dom";
+
+const Register = () => {
+  const [firstName, setfirstName] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error,setError] = useState("");
+  
+
+// http://localhost:5000/users/
+
+  return
+};
+  export default Register;
