4282fbf3085d0be111bcaa67a0fa2a2271ba0548
AhmadMraish <ahmadmraish59@gmail.com>
2021-09-10 16:03:18 +0300

    Navigation Switch Routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,24 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 const App = () => {
-	return <div className="App">App component</div>;
+	return(
+		<>
+		<Navigation> </Navigation>
+		<div className="App">
+		 <Switch>
+		 <Route exact path="/Login" render={() => <Login/>}/>
+		 <Route exact path="/Home" render={() => <Home/>}/>
+		 <Route exact path="/Register"/>
+		 <Route exact path="/Dashboard" component={()=> <Dashboard/>} />
+		 
+		 <Route path="*" component={() => "404 Page Not Found"} />
+		 </Switch>
+
+
+
+		</div>
+		</>
+	)
 };
 
 export default App;
