52ee2dc12c5837d5a2c9ac0d624ee6a396a5748b
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-09-10 21:05:36 +0300

    prepare routing in frontend files

--- frontend/src/components/dashboard/index.js
+++ frontend/src/components/dashboard/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Dashboard = () => {
-	return <div className="App">Dashboard component</div>;
+	return (<div className="App">
+		
+        <Navigation> </Navigation>
+		<left/>
+		<Switch>
+            <Route exact path="/timeline" component={Dashboard} />
+            <Route exact path="/album" component={Dashboard} />
+            <Route exact path="/follwers" component={Dashboard} />
+            <Route exact path="/updateUserInfo" component={Dashboard} />
+        </Switch>
+		<right/>		
+		</div>
+		);
 };
 
 export default Dashboard;
---
+++ frontend/src/contexts/context.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthProvider=()=>{
+
+}
+
+export default AuthProvider;
