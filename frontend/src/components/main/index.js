58d67423fd3ae4e891429f293f66955ea30c49e8
BasharAlZawahreh <86913003+BasharAlZawahreh@users.noreply.github.com>
2021-09-10 23:52:55 +0300

    design main component

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
-
+import './main.css'
 const Main = () => {
-	return <div className="App">App component</div>;
+	return(
+		
+		<div className="main">
+		
+		<div class="post_area">
+                <div class="user_Info_PA">
+					<img src="/assest/avatar2.jpg" alt=""/>
+                    <div>
+                        <p id="post_user">Naif</p>
+                    </div>
+                </div>
+                <div class="post_input">
+                    <textarea id="posts" rows="3"></textarea>
+                    <button id="create_post">Post</button>
+                </div>
+            </div>
+		</div>
+
+	) 
 };
 
 export default Main;
