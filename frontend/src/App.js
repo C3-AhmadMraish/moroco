0a54b38b9234057d91643ed463944cd27babace2
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-09-11 00:06:51 +0300

    some changes in routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
 import { Route ,Switch} from "react-router-dom";
+import Cover from "./components/cover/cover";
+import Header from "./components/header/index";
 // import Navigation from "./components/Navigation/Navigation";
 // import SignUp from "./components/auth/Signup/Signup";
 // import Login from "./components/auth/login/Login";
 // import AuthProvider from "./contexts/context";
-import Dashboard from "./components/dashboard";
+
 const App = () => {
   return (
+	  <>
+		  {/* <AuthProvider> */}
     <div className="App">
-      <Switch>
-        {/* <Route exact path="/" render={() => <Login />} /> */}
-        {/* <Route exact path="/SignUp" component={SignUp} /> */}
-        {/* <AuthProvider> */}
-          <Route exact path="/" component={Dashboard} />
-        {/* </AuthProvider> */}
-        <Route path="*" component={() => "404 Page Not Found"} />
+	      <Header/>
+		  <Cover/>
+        <Switch>
+        {/* <Route exact path="/" render={() => <Login />} />
+        <Route exact path="/SignUp" component={SignUp} /> */}
+          {/* <Route exact path="/" component={Dashboard} /> */}
+        {/* <Route path="*" component={() => "404 Page Not Found"} /> */}
+
       </Switch>
     </div>
+        {/* </AuthProvider> */}
+		</>
   );
 };
 
