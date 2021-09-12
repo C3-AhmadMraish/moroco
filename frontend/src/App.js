504c9ffba2a6e408fcedac2482187315ac5cd525
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-09-12 23:29:09 +0300

    refactor App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,63 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Main from "./components/main/index";
+import React, { useContext, useState, useEffect, createContext } from "react";
+import {
+  useHistory,
+  Route,
+  Switch,
+  BrowserRouter,
+  useLocation,
+} from "react-router-dom";
 import SignUp from "./components/auth/signUp/Signup";
 import "./App.css";
 import Login from "./components/auth/login/Login";
+import Search from "./components/search/search";
+import Post from "./components/post/post";
+import Feed from "./components/feed/feed";
+import Header from "./components/header";
+import Cover from "./components/cover/cover";
+import LeftSideBar from "./components/leftSideBar/leftSideBar";
+import RightSideBar from "./components/rightSideBar/rightSideBar";
+import "./components/main/main.css";
+import "./App.css";
+import { AuthContext } from "./contexts/context";
+export const postContext = createContext({ value: [], setValue: () => {} });
 const App = () => {
+  let { setIsLoggedIn, isLoggedIn, saveToken } = useContext(AuthContext);
+  const location = useLocation();
+  const history = useHistory();
+  const [value, setValue] = useState([]);
   return (
     <>
       <div>
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={SignUp} />
-          <Route path="*" component={() => "404 Page Not Found"} />
-        </Switch>
+        {!isLoggedIn ? (
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={SignUp} />
+            {/* <Route path="*" component={() => "404 Page Not Found"} /> */}
+            {/* <Route exact path="/search" component={Search} /> */}
+          </Switch>
+        ) : (
+          <>
+            <Header />
+            <Cover />
+            <div className="App">
+              <LeftSideBar />
+              <div className="main">
+                <Switch>
+                  <Route exact path="/search">
+                    <Search />
+                  </Route>
+                  <Route exact path="/">
+                    <postContext.Provider value={{ value, setValue }}>
+                      <Feed />
+                      <Post />
+                    </postContext.Provider>
+                  </Route>
+                </Switch>
+              </div>
+              <RightSideBar />
+            </div>
+          </>
+        )}
       </div>
     </>
   );
--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect, createContext } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory,Route,Switch,BrowserRouter,useLocation } from "react-router-dom";
 import "./main.css";
 import Post from "../post/post";
 import Feed from "../feed/feed";
@@ -10,9 +10,12 @@ import RightSideBar from "../rightSideBar/rightSideBar";
 import "../main/main.css";
 import "../../App.css";
 import { AuthContext } from "../../contexts/context";
+import SignUp from "../auth/signUp/Signup";
+import Search from "../search/search";
 export const postContext = createContext({ value: [], setValue: () => {} });
 const Main = () => {
   let { setIsLoggedIn, isLoggedIn, saveToken } = useContext(AuthContext);
+  const location=useLocation();
   const history = useHistory();
   const [value, setValue] = useState([]);
   return (
@@ -24,10 +27,20 @@ const Main = () => {
           <div className="App">
             <LeftSideBar />
             <div className="main">
+              <Switch>
+
+
+              <Route exact path="/search">
+              <Search/>
+              </Route>
+              <Route exact path="/">
               <postContext.Provider value={{ value, setValue }}>
-                <Feed />
-                <Post />
-              </postContext.Provider>
+                   <Feed />
+                   <Post />
+                 </postContext.Provider>
+              
+              </Route>             
+              </Switch>
             </div>
             <RightSideBar />
           </div>
