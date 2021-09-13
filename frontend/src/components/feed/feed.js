7c3395a5bea2fc6f1326c7e51f5b9ce5714944cb
AbdullahNaser94 <abdulahnaser94@gmail.com>
2021-09-13 22:22:21 +0300

    add function nameUsersFeed in file feed.js

--- frontend/src/components/feed/feed.js
+++ frontend/src/components/feed/feed.js
@@ -1,4 +1,4 @@
-import React, {  useState ,useContext} from 'react';
+import React, {  useState ,useContext, useEffect} from 'react';
 import './feed.css';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import axios from 'axios';
@@ -6,23 +6,39 @@ import { AuthContext } from '../../contexts/context';
 import { postContext } from '../../App';
 const Feed = () => {
     const [postBody,setPostBody]=useState([]);
-    const { validateToken, token, isLoggedIn } = useContext(AuthContext);
+    const { validateToken, token, isLoggedIn , userId } = useContext(AuthContext);
+    const [nameUserFeed, setNameUserFeed] = useState("");
     const {value,setValue}=useContext(postContext);
         const Addpost=()=>{
             axios.post("http://localhost:5000/posts",{body:postBody},{
             headers: {
               Authorization: `Bearer ${token}`,
             }}).then(result=>setValue([...value,result]))
-           
         }
-
-	return(
-		<div className="Feed">
-		<div className="post_area">
+        const nameUsersFeed = async () => {
+            try {
+                const res = await axios.get(`http://localhost:5000/users/${userId}`,
+                {
+                    headers: {
+                      Authorization: `Bearer ${token}`,
+                    }}
+                )
+                console.log(res.data.posts.firstName);
+                setNameUserFeed(res.data.posts.firstName)
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        useEffect(() => {
+            nameUsersFeed();
+          }, []);
+    return(
+        <div className="Feed">
+        <div className="post_area">
                 <div className="user_Info_PA">
-					<img src="/assets/avatar3.png" alt=""/>
+                    <img src="/assets/avatar3.png" alt=""/>
                     <div>
-                        <p id="post_user">Naif</p>
+                        <p id="post_user">{nameUserFeed}</p>
                     </div>
                 </div>
                 <div className="post_input">
@@ -30,8 +46,7 @@ const Feed = () => {
                     <button onClick={Addpost} id="create_post"><AddCircleOutlineIcon/></button>
                 </div>
             </div>
-		</div>
-	) 
+        </div>
+    ) 
 };
-
 export default Feed;
