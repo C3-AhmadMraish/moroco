11876237e2f57f7885076b1afd45955b33962710
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-09-13 11:41:26 +0300

    create function to delete posts

--- frontend/src/components/post/post.js
+++ frontend/src/components/post/post.js
@@ -1,84 +1,101 @@
 import React, { useEffect, useState, useContext } from "react";
 import "./post.css";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 
 import axios from "axios";
 import { postContext } from "../../App";
-
+import { AuthContext } from "../../contexts/context";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import CommentIcon from "@material-ui/icons/Comment";
 const Post = () => {
   const [posts, setPosts] = useState([]);
-  const { value } = useContext(postContext);
+  const { value, setValue } = useContext(postContext);
+  const { token } = useContext(AuthContext);
+  const choose = (body) => {
+    const arraybody = body.split(" ");
 
-  const choose=(body)=>{
-
-    const arraybody=body.split(" ");
-
-    return<span>{ arraybody.map(x=>{
-      
-      if(x.startsWith("http")){
-        // console.log(x.toString());
-        return <img className="postImg" src={x} />
-      }
-      return x+" ";
-
-    })}</span>
-
-  }
+    return (
+      <span>
+        {arraybody.map((x) => {
+          if (x.startsWith("http")) {
+            return <img className="postImg" src={x} />;
+          }
+          return x + " ";
+        })}
+      </span>
+    );
+  };
+  const getAllPosts = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/posts");
+      console.log(res.data.posts);
+      setPosts(res.data.posts);
+    } catch (error) {
+      setPosts([]);
+      console.log(error);
+    }
+  };
   useEffect(() => {
-    axios
-    .get("http://localhost:5000/posts")
-    .then((res) =>{
-     setPosts(res.data.posts)});
+    getAllPosts();
   }, [value]);
+
+  const deletePost = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/posts/deletepost/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      getAllPosts();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="colreverse">
-    <>
-      {posts &&
-        posts.map((p, i) => (
-          <div key={i} className="post">
-            <div className="postdevid">
-              <div className="postTop">
-                <div className="postTopLeft">
-                  <img
-                    className="postProfileImg"
-                    src="/assets/avatar3.png"
-                    alt=""
-                  />
-                  <span className="postUsername">NAif</span>
-                  <span className="postDate">3 hour ago</span>
+      <>
+        {posts &&
+          posts.map((p, i) => (
+            <div key={i} className="post">
+              <div className="postdevid">
+                <div className="postTop">
+                  <div className="postTopLeft">
+                    <img
+                      className="postProfileImg"
+                      src="/assets/avatar3.png"
+                      alt=""
+                    />
+                    <span className="postUsername">NAif</span>
+                    <span className="postDate">3 hour ago</span>
+                  </div>
+                  <div className="postTopRight">
+                    <span className="postDate">
+                      <HighlightOffIcon
+                        onClick={() => deletePost(p._id)}
+                        className="deleteIcon"
+                      />
+                    </span>
+                  </div>
                 </div>
-                <div className="postTopRight">
-                  <span className="postDate">
-                    <HighlightOffIcon className="deleteIcon" />
-                  </span>
-                </div>
-              </div>
-              <div className="postCenter">
-
-{choose(p.body)}
-                
-              </div>
-              <div className="postBottom">
-                <div className="postBottomLeft">
-                  <span className="postLikeIcon">
-                    <ThumbUpAltIcon />
-                  </span>
-                  <span className="postLikeCounter">32 people like it</span>
-                </div>
-                <div className="postBottomRight">
-                  <span className="postCommentIcom">
-                    <CommentIcon />
-                  </span>
+                <div className="postCenter">{choose(p.body)}</div>
+                <div className="postBottom">
+                  <div className="postBottomLeft">
+                    <span className="postLikeIcon">
+                      <ThumbUpAltIcon />
+                    </span>
+                    <span className="postLikeCounter">32 people like it</span>
+                  </div>
+                  <div className="postBottomRight">
+                    <span className="postCommentIcom">
+                      <CommentIcon />
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-    </>
+          ))}
+      </>
     </div>
   );
 };
