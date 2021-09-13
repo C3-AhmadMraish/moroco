414cfee95c7c3ccd29373e5032ec3d02845b54b1
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-09-13 22:19:02 +0300

    convert the left SideBar to links

--- frontend/src/components/leftSideBar/leftSideBar.js
+++ frontend/src/components/leftSideBar/leftSideBar.js
@@ -1,6 +1,7 @@
 import React, {useState, useContext , useEffect } from "react";
 import './leftSideBar.css';
 import axios from "axios";
+import { Link } from 'react-router-dom';
 import RssFeedIcon from '@material-ui/icons/RssFeed';
 import PhotoAlbumIcon from '@material-ui/icons/PhotoAlbum';
 import PeopleIcon from '@material-ui/icons/People';
@@ -41,8 +42,8 @@ const LeftSideBar = () => {
                 <span>{nameUser}</span>
             </div>
             <ul className="listIcon">
-                <li><RssFeedIcon/> timeline</li>
-                <li><EditIcon/> Edit profile</li>
+            <Link style={{textDecoration:"none"}} to="/"><li><RssFeedIcon/> timeline</li></Link>
+                <Link style={{textDecoration:"none"}} to="/edit"><li><EditIcon/> Edit profile</li></Link>
                 <li><PhotoAlbumIcon/> Album</li>
                 <li><PeopleIcon/> Followers</li>
             </ul>
