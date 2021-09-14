c85a928d6f8e98ec7efdc924c2794a0c35ad06e7
AhmadMraish <ahmadmraish59@gmail.com>
2021-09-14 07:45:20 +0300

    Fixed album css, add buttons and comments on whats needed as functionality, working on backend

--- frontend/src/components/leftSideBar/Album/Album.js
+++ frontend/src/components/leftSideBar/Album/Album.js
@@ -5,14 +5,29 @@ import Images from "./Images";
 const Album = () => {
 const [selectedImg, setSelectedImg]= useState(Images[0])
 
+//To do make img overlay on top of selected image to show upload,change cover, change prof buttons
+// buttons will not render on image area
+// dont use vertical images!
+
+ //  const getAllImgs = async () => {
+ //  const updateUserCover = async () => {
+ //  const updateUserAvatar = async () => {
+
+
+//add on click events to buttons bellow and propper mapping to db
     return (
 
         <div className="Album">
             <div className="container">
                 <img src = {selectedImg} alt="Selected" ClassName="selected"/>
+                <button>Upload Image</button>
+                <button>Change Profile Photo</button>
+                <button>Change Cover Photo</button>
                 <div classname="imgContainer">
                     {Images.map((img, index)=> (
                         <img
+                        width= "250px"
+                        height="250px"
                         style={{border:selectedImg === img ? "4px solid purple" : "" }}
                         key = {index}
                         src = {img}
