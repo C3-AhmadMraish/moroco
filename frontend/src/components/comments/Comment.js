b75b54fccfc9426a9dc83b5c2a345041b0110782
BasharAlZawahreh <86913003+BasharAlZawahreh@users.noreply.github.com>
2021-09-16 07:26:43 +0300

    comment compnent try1

---
+++ frontend/src/components/comments/Comment.js
@@ -0,0 +1,122 @@
+import React, { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import "./Comments.css";
+import TimeAgo from "react-timeago";
+import frenchStrings from "react-timeago/lib/language-strings/en";
+import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
+import { AuthContext } from "../../contexts/context";
+
+const Comment = ({ c }) => {
+  const formatter = buildFormatter(frenchStrings);
+  const { token, userId } = useContext(AuthContext);
+  const [post, setpost] = useState();
+  const [NewBody, setNewBody] = useState(""); //For update
+  const [newComment, setnewComment] = useState();
+  const isAuthorized = c.commenter._id === userId;
+
+  const updateComment = async (id) => {
+    try {
+      const res = await axios.put(
+        `http://localhost:5000/posts/comment/${id}`,
+        { body: NewBody },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteComment = async (id) => {
+    try {
+      const res = await axios.delete(
+        `http://localhost:5000/posts/${postId}/comment/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (isAuthorized) {
+    return (
+      <div
+        key={c._id}
+        className="commentSection"
+        style={{ marginLeft: "70px", marginTop: "30px" }}
+      >
+        <div className="postdevid">
+          <div className="postTop">
+            <div className="postTopLeft">
+              <img className="postProfileImg" src={c.commenter.avatar} alt="" />
+              <span className="postUsername">{c.commenter.firstName}</span>
+              <span className="postDate">
+                <TimeAgo date={c.date} formatter={formatter} />
+              </span>
+            </div>
+            <div className="postTopRight">
+              <button
+                onClick={() => {
+                  deleteComment(c._id);
+                }}
+              >
+                Delete
+              </button>
+
+              <button
+                onClick={() => {
+                  updateComment(c._id);
+                }}
+              >
+                Update
+              </button>
+            </div>
+          </div>
+          <div className="postCenter">
+            <input
+              type="text"
+              onChange={(e) => {
+                setNewBody(e.target.value);
+              }}
+              style={{ background: "none", border: "none" }}
+              placeholder={c.comment}
+            />
+          </div>
+        </div>
+        <hr style={{ fontSize: "20px", color: "gray", marginTop: "30px" }} />
+      </div>
+    );
+  }
+
+  return (
+    <div
+      key={c._id}
+      className="commentSection"
+      style={{ marginLeft: "70px", marginTop: "30px" }}
+    >
+      <div className="postdevid">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img className="postProfileImg" src={c.commenter.avatar} alt="" />
+            <span className="postUsername">{c.commenter.firstName}</span>
+            <span className="postDate">
+              <TimeAgo date={c.date} formatter={formatter} />
+            </span>
+          </div>
+          <div className="postTopRight"></div>
+        </div>
+        <div className="postCenter">{c.comment}</div>
+      </div>
+      <hr style={{ fontSize: "20px", color: "gray", marginTop: "30px" }} />
+    </div>
+  );
+};
+
+export default Comment;
--- frontend/src/components/comments/Comments.js
+++ frontend/src/components/comments/Comments.js
@@ -7,16 +7,16 @@ import TimeAgo from "react-timeago";
 import frenchStrings from "react-timeago/lib/language-strings/en";
 import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import Comment from './Comment'
+
 const Comments = () => {
   const formatter = buildFormatter(frenchStrings);
   const { token, userId } = useContext(AuthContext);
   const [post, setpost] = useState();
-  const [NewBody, setNewBody] = useState(""); //For update
   const [comments, setComments] = useState(); // For create new
   const [newComment, setnewComment] = useState();
   const { postId } = useParams();
-  
-  
+
   const getAllComments = () => {
     axios
       .get(`http://localhost:5000/posts/getpostbyid/${postId}`, {
@@ -38,8 +38,6 @@ const Comments = () => {
     getAllComments();
   }, []);
 
-  
-
   const createNewComment = async () => {
     try {
       const res = await axios.post(
@@ -53,45 +51,6 @@ const Comments = () => {
       );
 
       setComments([...comments, res.data.commentAdded]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const updateComment = async (id) => {
-    try {
-      const res = await axios.put(
-        `http://localhost:5000/posts/comment/${id}`,
-        { body: NewBody },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      console.log(NewBody);
-      console.log(res.data.comments);
-
-      getAllComments();
-
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const deleteComment = async (id) => {
-    try {
-      const res = await axios.delete(
-        `http://localhost:5000/posts/${postId}/comment/${id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      getAllComments()
     } catch (error) {
       console.log(error);
     }
@@ -131,92 +90,7 @@ const Comments = () => {
         <hr style={{ fontSize: "20px", color: "gray", marginTop: "30px" }} />
         {comments &&
           comments.map((c) => {
-            const isAuthorized = c.commenter._id === userId;
-
-            return isAuthorized ? (
-              <div
-                key={c._id}
-                className="commentSection"
-                style={{ marginLeft: "70px", marginTop: "30px" }}
-              >
-                <div className="postdevid">
-                  <div className="postTop">
-                    <div className="postTopLeft">
-                      <img
-                        className="postProfileImg"
-                        src={c.commenter.avatar}
-                        alt=""
-                      />
-                      <span className="postUsername">
-                        {c.commenter.firstName}
-                      </span>
-                      <span className="postDate">
-                        <TimeAgo date={c.date} formatter={formatter} />
-                      </span>
-                    </div>
-                    <div className="postTopRight">
-                      <button
-                        onClick={() => {
-                          deleteComment(c._id);
-                        }}
-                      >
-                        Delete
-                      </button>
-
-                      <button
-                        onClick={() => {
-                          updateComment(c._id);
-                        }}
-                      >
-                        Update
-                      </button>
-                    </div>
-                  </div>
-                  <div className="postCenter">
-                    <input
-                      type="text"
-                      onChange={(e) => {
-                        setNewBody(e.target.value);
-                      }}
-                      style={{ background: "none", border: "none" }}
-                      placeholder={c.comment}
-                    />
-                  </div>
-                </div>
-                <hr
-                  style={{ fontSize: "20px", color: "gray", marginTop: "30px" }}
-                />
-              </div>
-            ) : (
-              <div
-                key={c._id}
-                className="commentSection"
-                style={{ marginLeft: "70px", marginTop: "30px" }}
-              >
-                <div className="postdevid">
-                  <div className="postTop">
-                    <div className="postTopLeft">
-                      <img
-                        className="postProfileImg"
-                        src={c.commenter.avatar}
-                        alt=""
-                      />
-                      <span className="postUsername">
-                        {c.commenter.firstName}
-                      </span>
-                      <span className="postDate">
-                        <TimeAgo date={c.date} formatter={formatter} />
-                      </span>
-                    </div>
-                    <div className="postTopRight"></div>
-                  </div>
-                  <div className="postCenter">{c.comment}</div>
-                </div>
-                <hr
-                  style={{ fontSize: "20px", color: "gray", marginTop: "30px" }}
-                />
-              </div>
-            );
+            <Comment c={c} />;
           })}
       </div>
     </div>
