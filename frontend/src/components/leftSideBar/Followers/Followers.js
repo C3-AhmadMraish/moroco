4d40961bf1218f9d3bd14c0c1c5f0b4c0bb84f41
AhmadMraish <ahmadmraish59@gmail.com>
2021-09-17 03:48:47 +0300

    create Followers component

---
+++ frontend/src/components/leftSideBar/Followers/Followers.js
@@ -0,0 +1,11 @@
+import React,{useState,useContext, useEffect} from "react";
+import { AuthContext } from "../../../contexts/context";
+import axios from "axios";
+
+const Followers = () => {
+return (
+    <>
+    </>
+)
+}
+export default Followers
