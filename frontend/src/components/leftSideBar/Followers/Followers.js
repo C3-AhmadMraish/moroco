2dce772745079a43ebc32140b86f03528dc2d731
AhmadMraish <ahmadmraish59@gmail.com>
2021-09-17 04:13:43 +0300

    followers functionality thinking

--- frontend/src/components/leftSideBar/Followers/Followers.js
+++ frontend/src/components/leftSideBar/Followers/Followers.js
@@ -3,8 +3,23 @@ import { AuthContext } from "../../../contexts/context";
 import axios from "axios";
 
 const Followers = () => {
+    const [follower,setFollower] = useState([])
+    //useEffect in app which has a dependancy followers if followers change it will refire /reupdate ( cus maybe some will follow me
+    //while i am using the website) then
+    //useeffect in Followers component, gets followers from user res.data.posts.followers
+    //put followers in the setFollowers usestate
+    //in return
+    // map over followers and return follower name and follower img
 return (
     <>
+{/*  map over follower Array (){
+         //render res.data.follower
+         / <br/>
+         // re render
+
+
+
+}*/}
     </>
 )
 }
