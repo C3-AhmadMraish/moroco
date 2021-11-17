1fde6173f6eb72c5099693a739961098b84177f0
AhmadMraish <ahmadmraish59@gmail.com>
2021-11-17 09:44:47 +0200

    Bashars fix for trending

--- backend/routers/controllers/trending.js
+++ backend/routers/controllers/trending.js
@@ -5,9 +5,9 @@ const { deleteOne } = require("../../db/models/trending");
 const getTrends = (req, res) => {
   Trend.find({})
     .populate("post")
-    .then( async(result) => {
+    .then(async (result) => {
       if (result.length < 3) {
-        await Trend.remove({})
+        await Trend.remove({});
         const trends = [];
         Post.find({})
           .sort({ likesCounter: -1 })
@@ -33,7 +33,7 @@ const getTrends = (req, res) => {
             newtrend1.save();
             newtrend2.save();
             newtrend3.save();
-
+            // console.log("status 1" + res.status);
             return res.status(201).json({
               success: true,
               message: `Trending posts.`,
@@ -41,83 +41,124 @@ const getTrends = (req, res) => {
             });
           })
           .catch((err) => {
-            console.log("err" + err);
+            console.log("err 1" + err);
           });
       }
-
+      // console.log("status 2" + res.status);
       return res.status(200).json({
         success: true,
         message: `Trending posts.`,
         trends: result,
       });
+    })
+    .catch((err) => {
+      console.log("err 2" + err);
+    });
+};
+
+const reGetTrends = (req, res) => {
+  Trend.find({})
+    .populate("post")
+    .then(async (result) => {
+      await Trend.remove({});
+      const trends = [];
+      Post.find({})
+        .sort({ likesCounter: -1 })
+        .limit(3)
+        .then((resultP) => {
+          trends.push(resultP[0]);
+          trends.push(resultP[1]);
+          trends.push(resultP[2]);
+
+          const newtrend1 = new Trend({
+            post: resultP[0]._id,
+            likesCounter: resultP[0].likesCounter,
+          });
+          const newtrend2 = new Trend({
+            post: resultP[1]._id,
+            likesCounter: resultP[1].likesCounter,
+          });
+          const newtrend3 = new Trend({
+            post: resultP[2]._id,
+            likesCounter: resultP[2].likesCounter,
+          });
+
+          newtrend1.save();
+          newtrend2.save();
+          newtrend3.save();
+          // console.log("status 1" + res.status);
+          return res.status(201).json({
+            success: true,
+            message: `Trending posts.`,
+            trends: trends,
+          });
+        })
+        .catch((err) => {
+          console.log("err 1" + err);
+        });
+     // console.log("status 2" + res.status);
+      // return res.status(200).json({
+      //   success: true,
+      //   message: `Trending posts.`,
+      //   trends: result,
+      // });
+    })
+    .catch((err) => {
+      console.log("err 2" + err);
     });
 };
 
 const addNewAndDeleteOld = (req, res) => {
   const postId = req.params.id;
-  let isChanged = false;
+  // let isChanged = false;
   //Check if post exists in trends
   Trend.findOne({ post: postId })
     .populate("post")
     .then(async (result) => {
-
       if (!result) {
-        let i =0
         const trends = await Trend.find({}).populate("post");
         const post = await Post.findOne({ _id: postId });
-       while (!trends.includes(postId) && i<3) {
-        console.log(isAdded,"isAdded")
+        for (let i = 0; i < trends.length; i++) {
           if (trends[i].post.likesCounter < post.likesCounter) {
-            const _id = await trends[i].post._id;
-            Trend.findByIdAndDelete(_id).then((result) => {
-              console.log("delete" + result);
-            });
-
-            const newTrend = await new Trend({ post: postId });
-            
-            newTrend.save();
-            trends.push(postId)
+            return reGetTrends(req, res);
+            // console.log("before break")
+            // break;
           }
-          i++
-          // isChanged=true;
-          
-        };
-        return res.status(200).json({
+        }
+      } else {
+        // console.log("status 4" + res.status);
+        return res.status(201).json({
           success: true,
-          changedTrend: true,
+          changedTrend: false,
         });
-        
       }
     })
     .catch((err) => {
       console.log(err);
+      // console.log("status 5" + res.status);
       return res.status(500).json({
         success: false,
         message: `Eroor`,
       });
     });
-  // get post by id which is comming from req
-  // loop over trends and compare each with post.likesCounter
-  // if post.likesCounter > one of them >>> then replace them
 };
 
 const shoshoDelete = (req, res) => {
-  console.log("we entered shosho");
+  // console.log("we entered shosho");
   // we enetered here
   const _id = req.params.id;
-  console.log("shoshoDelete", _id);
-  Trend.findOne({post:_id})
+  // console.log("shoshoDelete", _id);
+  Trend.findOne({ post: _id })
     .then((result) => {
-      console.log("bashar",result)
+      console.log("bashar", result);
       if (result) {
-        console.log("beforedeleteone",result)
+        // console.log("beforedeleteone", result);
         Trend.deleteOne({ post: _id }).then((result) => {
           // re-call getTrends   to replace deleted post
-          if (result){
-            console.log("after deleteone result", result)
-            getTrends()
-          }
-         ; //headers problem
+          if (result) {
+            // console.log("after deleteone result", result);
+            getTrends(req, res);
+          } //headers problem
         });
       }
     })
